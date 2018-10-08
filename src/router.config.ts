e1500613db7758f5e28d9245ed44560bd06ab6d6
jpb <jpb.06@outlook.com>
2018-10-08 20:51:11 +0200

    Cleaning up routes config

--- src/router.config.ts
+++ src/router.config.ts
@@ -1,22 +1,15 @@
 import { Routes } from "@angular/router";
-import { HomeComponent } from "./app/home/home.component";
-//import { AboutComponent } from "./app/about/about.component";
 
+import { HomeComponent } from "./app/home/home.component";
+import { LoginComponent } from "./app/login/login.component";
+import { GuildHomeComponent } from "./app/guild-home/guild-home.component";
+
+import { AuthGuard } from "./logic/auth.guard";
 
 export const routerConfig: Routes = [
-  {
-    path: 'home',
-    component: HomeComponent
-  },
-  //{
-  //  path: 'guild',
-  //  component: GuildSelectorComponent 
-  //},
-  //{
-  //  path: 'about',
-  //  component: AboutComponent,
-  // // canActivate: [CanActivateGuildConfig]
-  //},
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'guild', component: GuildHomeComponent, canActivate: [AuthGuard] },
   {
     path: '',
     redirectTo: '/home',
