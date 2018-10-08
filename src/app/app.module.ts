a7adc70a6cdc681812eef9141edf9174f6b49efc
jpb <jpb.06@outlook.com>
2018-10-08 20:51:48 +0200

    Cleaning up app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,33 +9,24 @@ import { routerConfig } from "../router.config";
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-//import { GuildSelectorComponent } from './guild-selector/guild-selector.component';
-//import { AboutComponent } from './about/about.component';
-//import { GuildSettingsComponent } from './guild-settings/guild-settings.component';
-//import { GuildRegionsComponent } from './guild-regions/guild-regions.component';
-//import { GuildFactionsComponent } from './guild-factions/guild-factions.component';
-//import { GuildPlayersComponent } from './guild-players/guild-players.component';
 import { LoginComponent } from './login/login.component';
 import { AlertComponent } from './alert/alert.component';
 
-//import { HTTP_INTERCEPTORS } from '@angular/common/http';
-//import { AuthInterceptor } from './../logic/auth.interceptor';
-//import { AuthGuard } from '../logic/auth.guard';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthInterceptor } from './../logic/auth.interceptor';
+
+import { AuthGuard } from '../logic/auth.guard';
 import { AlertService } from '../services/alert.service';
 import { AuthenticationService } from '../services/authentication.service';
+import { GuildHomeComponent } from './guild-home/guild-home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    //GuildSelectorComponent,
-    //AboutComponent,
-    //GuildSettingsComponent,
-    //GuildRegionsComponent,
-    //GuildFactionsComponent,
-    //GuildPlayersComponent,
     LoginComponent,
-    AlertComponent
+    AlertComponent,
+    GuildHomeComponent
   ],
   imports: [
     BrowserModule,
@@ -45,14 +36,10 @@ import { AuthenticationService } from '../services/authentication.service';
     RouterModule.forRoot(routerConfig)
   ],
   providers: [
-    //AuthGuard,
+    AuthGuard,
     AlertService,
     AuthenticationService,
-    //{
-    //provide: HTTP_INTERCEPTORS,
-    //  useClass: AuthInterceptor,
-    //multi: true
-    //}
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
   ],
   bootstrap: [
     AppComponent
