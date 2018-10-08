b187c445f3e76f8e906905ac091e4e152997c7ea
jpb <jpb.06@outlook.com>
2018-10-08 20:51:33 +0200

    Logout button in main menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import * as $ from "jquery"
+import { AuthenticationService } from '../services/authentication.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  isCollapsed = true;
+export class AppComponent implements OnInit {
+
+  constructor(
+    private authenticationService: AuthenticationService,
+    private router: Router
+  ) { }
+
+  ngOnInit() { }
+
+  logout() {
+    this.authenticationService.logout();
+    this.router.navigate(['home']);
+  }
 }
