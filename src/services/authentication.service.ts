03e82c91cb95025521ffb09546d2bd29790c08e2
jpb <jpb.06@outlook.com>
2018-10-08 20:50:11 +0200

    Using the async way for auth service login

--- src/services/authentication.service.ts
+++ src/services/authentication.service.ts
@@ -1,28 +1,24 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
 import * as moment from "moment";
-import { Observable } from 'rxjs';
 
 @Injectable()
 export class AuthenticationService {
   constructor(private http: HttpClient) { }
 
-  login(guildId: string, password: string): Observable<boolean> {
-    // ${config.apiUrl}
-    return this.http.post<any>(`http://localhost:1337/api/ws/login`, { login: guildId, password: password })
-      .pipe(map(data => {
-        // login successful if there's a jwt token in the response
-        if (data && data.status === 'success' && data.token) {
-          // store user details and jwt token in local storage to keep user logged in between page refreshes
-          const expiresAt = moment().add(data.expiresIn, 'second');
+  async login(guildId: string, password: string) {
 
-          localStorage.setItem('id_token', data.token);
-          localStorage.setItem("expires_at", JSON.stringify(expiresAt.valueOf()));
-        }
+    let req = await this.http.post<any>(`http://localhost:1337/api/ws/login`, { login: guildId, password: password }).toPromise();
 
-        return true;
-      }));
+    if (req && req.status === 'Success' && req.token) {
+      // store user details and jwt token in local storage to keep user logged in between page refreshes
+      const expiresAt = moment().add(req.expiresIn, 'second');
+
+      localStorage.setItem('id_token', req.token);
+      localStorage.setItem("expires_at", JSON.stringify(expiresAt.valueOf()));
+    }
+
+    return true;
   }
 
   logout() {
