6234d53255a18a11a3593ef248e1e37921a1d8be
jpb <jpb.06@outlook.com>
2018-10-09 21:39:15 +0200

    Placeholders for regions, factions and about pages

---
+++ src/app/guild-factions/guild-factions.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { GuildFactionsComponent } from './guild-factions.component';
+
+describe('GuildFactionsComponent', () => {
+  let component: GuildFactionsComponent;
+  let fixture: ComponentFixture<GuildFactionsComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ GuildFactionsComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(GuildFactionsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
