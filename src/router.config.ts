377e4a3cbcea9bc41ecabd088241a83fcb0b7a4f
jpb <jpb.06@outlook.com>
2018-10-09 21:42:30 +0200

    Adding placeholders to router config

--- src/router.config.ts
+++ src/router.config.ts
@@ -2,14 +2,20 @@ import { Routes } from "@angular/router";
 
 import { HomeComponent } from "./app/home/home.component";
 import { LoginComponent } from "./app/login/login.component";
+import { AboutComponent } from "./app/about/about.component";
 import { GuildHomeComponent } from "./app/guild-home/guild-home.component";
+import { GuildFactionsComponent } from "./app/guild-factions/guild-factions.component";
+import { GuildRegionsComponent } from "./app/guild-regions/guild-regions.component";
 
 import { AuthGuard } from "./logic/auth.guard";
 
 export const routerConfig: Routes = [
   { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
   { path: 'login', component: LoginComponent },
   { path: 'guild', component: GuildHomeComponent, canActivate: [AuthGuard] },
+  { path: 'factions', component: GuildFactionsComponent, canActivate: [AuthGuard] },
+  { path: 'regions', component: GuildRegionsComponent, canActivate: [AuthGuard] },
   {
     path: '',
     redirectTo: '/home',
