97085b681937bfa61cdafec73670e013cc0e368c
jpb <jpb.06@outlook.com>
2018-10-21 15:06:37 +0200

    Adding Guild config type

---
+++ src/types/api/guild.ts
@@ -0,0 +1,13 @@
+export declare class GuildConfiguration {
+  guildId: string;
+  messagesImage: string;
+  messagesFooterName: string;
+  scanMainRegionName: string;
+  mainChannelName: string;
+  adminChannelName: string;
+  emergencyChannelName: string;
+  acknowledged: string;
+  activityNoticeMinPlayers: number;
+  activityNoticeMessages: Array<string>;
+  commandsPrefix: string;
+}
