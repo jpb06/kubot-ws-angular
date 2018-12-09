a593453f582863823653a6c998ded72edaed0707
jpb <jpb.06@outlook.com>
2018-12-09 19:04:28 +0100

    Packing things up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,21 +13,27 @@ import { LoginComponent } from './login/login.component';
 import { AlertComponent } from './alert/alert.component';
 
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import { AuthInterceptor } from './../logic/auth.interceptor';
+import { AuthInterceptor } from './../security/auth.interceptor';
 
-import { AuthGuard } from '../logic/auth.guard';
+import { AdminAuthGuard } from '../security/guards/admin.auth.guard';
+import { GuildAuthGuard } from '../security/guards/guild.auth.guard';
+
 import { AlertService } from '../services/alert.service';
+import { DateService } from '../services/date.service';
+import { AdminService } from '../services/api/admin.service';
 import { AuthenticationService } from '../services/api/authentication.service';
 import { KubotService } from '../services/api/kubot.service';
+import { StaticDataService } from '../services/api/static.service';
+
 import { GuildHomeComponent } from './guild-home/guild.home.component';
 import { MenuGuestComponent } from './main-menu/menu-guest/menu.guest.component';
 import { MenuUserComponent } from './main-menu/menu-user/menu.user.component';
 import { AboutComponent } from './about/about.component';
 import { GuildFactionsComponent } from './guild-factions/guild.factions.component';
 import { GuildRegionsComponent } from './guild-regions/guild.regions.component';
-import { DateService } from '../services/date.service';
 import { WatchItemComponent } from './watch-item/watch.item.component';
 import { WatchedFactionSummaryComponent } from './watched-faction-summary/watched.faction.summary.component';
+import { AdminComponent } from './admin/admin.component';
 
 @NgModule({
   declarations: [
@@ -42,7 +48,8 @@ import { WatchedFactionSummaryComponent } from './watched-faction-summary/watche
     GuildFactionsComponent,
     GuildRegionsComponent,
     WatchItemComponent,
-    WatchedFactionSummaryComponent
+    WatchedFactionSummaryComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
@@ -53,10 +60,13 @@ import { WatchedFactionSummaryComponent } from './watched-faction-summary/watche
     RouterModule.forRoot(routerConfig)
   ],
   providers: [
-    AuthGuard,
+    AdminAuthGuard,
+    GuildAuthGuard,
     AlertService,
+    AdminService,
     AuthenticationService,
     KubotService,
+    StaticDataService,
     DateService,
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
   ],
--- src/router.config.ts
+++ src/router.config.ts
@@ -6,16 +6,19 @@ import { AboutComponent } from "./app/about/about.component";
 import { GuildHomeComponent } from "./app/guild-home/guild.home.component";
 import { GuildFactionsComponent } from "./app/guild-factions/guild.factions.component";
 import { GuildRegionsComponent } from "./app/guild-regions/guild.regions.component";
+import { AdminComponent } from "./app/admin/admin.component";
 
-import { AuthGuard } from "./logic/auth.guard";
+import { GuildAuthGuard } from "./security/guards/guild.auth.guard";
+import { AdminAuthGuard } from "./security/guards/admin.auth.guard";
 
 export const routerConfig: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'guild', component: GuildHomeComponent, canActivate: [AuthGuard] },
-  { path: 'factions', component: GuildFactionsComponent, canActivate: [AuthGuard] },
-  { path: 'regions', component: GuildRegionsComponent, canActivate: [AuthGuard] },
+  { path: 'guild', component: GuildHomeComponent, canActivate: [GuildAuthGuard] },
+  { path: 'factions', component: GuildFactionsComponent, canActivate: [GuildAuthGuard] },
+  { path: 'regions', component: GuildRegionsComponent, canActivate: [GuildAuthGuard] },
+  { path: 'admin', component: AdminComponent, canActivate: [AdminAuthGuard] },
   {
     path: '',
     redirectTo: '/home',
