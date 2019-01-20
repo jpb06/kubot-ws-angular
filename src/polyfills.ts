47e561484ff6893c48ebce3f6f19b8c5e48aeaf1
jpb <jpb.06@outlook.com>
2019-01-20 09:34:17 +0100

    Adding smoothscroll polyfill because edge is retarded

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -78,3 +78,8 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+
+import smoothscroll from 'smoothscroll-polyfill';
+
+// kick off the polyfill!
+smoothscroll.polyfill();
